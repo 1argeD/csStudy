3edce6c723c84d3c942f73fc2cc63c5255074e66
1argeD <gkswhdgur1234@naver.com>
2023-06-19 23:46:45 +0900

    <feat> 오토 박싱과 동일 타입 연산

--- src/main/java/오토박싱연산과동일타입연산.java
+++ src/main/java/오토박싱연산과동일타입연산.java
@@ -1,4 +1,5 @@
-public class CSTest {
+public class 오토박싱연산과동일타입연산 {
+
     public static void main(String[] args) {
         long t = System.currentTimeMillis();
         Long sum = 0L;
@@ -14,8 +15,10 @@ public class CSTest {
             mou+=1;
         }
 
-
-        System.out.println("오토 박싱 연상 : " + (System.currentTimeMillis() - t) + " ms");
+        /*Long에서 long으로  오토 박싱 연산 시 동일 타입 연산에 비해 속도가 느리고
+        * 100만 건 기준에서 는 약 5까지 차이가 난다
+        * */
+        System.out.println("오토 박싱 연산 : " + (System.currentTimeMillis() - t) + " ms");
         System.out.println("동일 타입 연산 : " + (System.currentTimeMillis() - t)  + " ms");
     }
 }
