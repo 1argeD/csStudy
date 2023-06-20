e891a5d223ad17b1d834449500a458d9dc4ac02c
1argeD <gkswhdgur1234@naver.com>
2023-06-20 16:28:33 +0900

    <feat> Object클래스

---
+++ src/main/java/com/example/cs/Object클래스.java
@@ -0,0 +1,25 @@
+package com.example.cs;
+
+public class Object클래스 {
+    // wait, notify, notifyAll
+
+    /*Java의 최상위 클래서 = Object 클래스
+    *
+    * Object Class 가 갖고 있는 메서드
+    *
+    * toString()
+    * hashCode(0
+    * wait() - 갖고 있던 고유 lock 해제, Thread를 잡들게 함
+    *
+    * notify()
+    * 잠들던 Thread 중 임의의 하나를 깨움.
+    *
+    * notifyAll()
+    *잠들어 있던 Tread 를  모두 깨움*/
+
+    //wait, notify, notifyAll
+
+    /* => Synchronized 블록 내에서 실행되어야 함.
+    *
+    *  => 그 블록 안에서 호출하는 경우  IllegalMonitorStateException 발생.*/
+}
