1340de34dd01ab90076442c7cca4a5621ae767c5
1argeD <gkswhdgur1234@naver.com>
2023-06-20 23:31:34 +0900

    <chore> 주석 수정

--- src/main/java/com/example/cs/오토박싱연산과동일타입연산.java
+++ src/main/java/com/example/cs/오토박싱연산과동일타입연산.java
@@ -18,7 +18,7 @@ public class 오토박싱연산과동일타입연산 {
         }
 
         /*Long에서 long으로  오토 박싱 연산 시 동일 타입 연산에 비해 속도가 느리고
-        * 100만 건 기준에서 는 약 5까지 차이가 난다*/
+        * 100만 건 기준에서 약 5배까지 차이가 난다*/
 
         System.out.println("오토 박싱 연산 : " + (System.currentTimeMillis() - t) + " ms");
         System.out.println("동일 타입 연산 : " + (System.currentTimeMillis() - t)  + " ms");
