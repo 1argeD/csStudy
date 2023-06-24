ed75827480c002c96d255b7615914fac372d9f27
1argeD <gkswhdgur1234@naver.com>
2023-06-24 23:05:52 +0900

    <feat> 자바 버전의 변화

---
+++ src/main/java/com/example/cs/JavaVer.java
@@ -0,0 +1,42 @@
+package com.example.cs;
+
+public class JavaVer {
+    //Java 8
+
+    /*
+        1. 함수형 프로그래밍 패러다임 적용
+            i. Lambda expression
+            ii.Stream
+            iii.Optional
+        2. interface 에서 default method 사용 가능
+        3. 새로운 Data and Time API
+        4. JVM 개선
+            i. JVM 에 의해 크기가 결정되던 Permanent Heap 삭제
+            ii. OS 가 자동 조정하는 Native 메모리 영역인 Metaspace 추가
+            iii. Default GC Serial GC -> Parallel GC (멀티 스레드 방식)
+    * */
+
+    //Java 9
+
+    /*
+        1.module
+        2.interface 에서 private method 사용 가능
+        3. Collection, Stream, Optional API 사용법 개선
+            i. ex) Immutable collection, Stream.ofNullable(), Optional.orElseGet()
+        4. Default GC Parallel GC -> G1GC(멀티 프로세서 환경에 적합)
+    * */
+
+    //Java 10
+
+    /*1.var (지역 변수 타입 추론)*/
+
+    //Java 11
+
+    /*
+        1.HTTP Client API
+            i. HTTP/2 지원
+            ii. RestTemplate 의 상위 호환
+        2. String API 사용법 개선
+        3. OracleJDK 독점 기능이 OpenJDK 에 포함
+    * */
+}
