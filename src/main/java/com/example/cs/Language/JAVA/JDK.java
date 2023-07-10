4954f616ab8a79d460d25525edaa35adf1e2d0dd
1argeD <gkswhdgur1234@naver.com>
2023-07-10 15:09:45 +0900

    <feat> JDK

---
+++ src/main/java/com/example/cs/Language/JAVA/JDK.java
@@ -0,0 +1,40 @@
+package com.example.cs.Language.JAVA;
+
+public class JDK {
+    //OpenJDK
+
+    /*
+    Open JDK
+    java 애플리케이션을 빌드하고 실행하려면 Java Platfrom Standard Edition("Java SE")
+    사양을 구현하는 Java 컴파일러, Java 런타임 라이브러리 및 가상 머신이 필요하다
+
+
+   OpenJDK는 Java SE 사양의 오픈 소스 참조 구현이지만 소스 코드일 뿐이다. 이진 배포판은
+   지원되는 여러 플랫폼에 대해 여러 공급업체에서 제고앟ㄴ다. 이러한 배포판은
+   라이선스, 상용 지원, 지원되는 플랫폼 및 업데이트 빈도가 다르다.
+    */
+
+    //2.Releases
+
+    /*
+    현재 JDK 릴르스 모델에서는 3월과 9월에 6개월마다 새로운 주요 버전 번호가 포함된
+    새로운 기능 릴리스가 계획되어 있다. 또한 분기별 버그 수정 업데이트가 있다.
+
+     3년마다 9월 릴리스는 LTS(장기 지원) 릴리스로, 최소 3년 동안 업데이트된다.
+     최신 LYS 버전을 유지할지 아니면 최신 기능 릴리스를 사용하고 6개월마다 업그레이들할지
+     결정해야 한다. 두 가지 방법 모두 좋다. 확실하지 않은 경우 최신 LTS 버전을 사용하면
+     된다
+
+    OpenKDK 프로젝트 자체는 사양, 소스 코드 및 메일링 목록을 찾을 수 있는
+    openjdk.java.net 에서 관리된다. 그런나 다운로드할 수 있는 빌드가 없다.
+     */
+
+    //3. Distributions
+
+    //1)OpenJDK builds by Oracle (jdk.java.net)
+
+    /*
+    Oracle은 Linux, macOS 및 Windows 용 OpenJDK 빌드를 압축된 아카이브형식으로
+
+    * */
+}
